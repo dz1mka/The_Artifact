b7844d99a85ccb404e79f8897330614ad0e4cfc3
dz1mka <dzimka@gmail.com>
2025-07-19 00:13:22 +0300

    49 - Enumerations

---
+++ Assets/Scripts/Bush Scripts/BushVisual.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushVisual : MonoBehaviour
+{
+    public enum BushVariant {Green, Cyan, Yellow }
+    private BushVariant bushVariant;
+
+    private void Start()
+    {
+     
+
+    }
+}
