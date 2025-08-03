190bd88fc58527e70b06145d033a54c386eb6c9c
dz1mka <dzimka@gmail.com>
2025-08-03 23:56:47 +0300

    59 - WolfAI Script Attacking The Bush And The Artifact

--- Assets/Scripts/Enemy Scripts/WolfAI.cs
+++ Assets/Scripts/Enemy Scripts/WolfAI.cs
@@ -54,7 +54,97 @@ public class WolfAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!artifact)
+            return;
+
+        if (isEater)
+        {
+            if (bushFruitsTarget && bushFruitsTarget.enabled && bushFruitsTarget.HasFruits() && !killingBush)
+            {
+                if (Vector2.Distance(transform.position, bushFruitsTarget.transform.position) > 0.5f)
+                {
+                    float step = moveSpeed * Time.deltaTime;
+
+                    transform.position = Vector2.MoveTowards(transform.position, bushFruitsTarget.transform.position, step);
+
+                    isMoving = true;
+                }
+                else
+                {
+                    isMoving = false;
+                    bushFruitsTarget.HarvestFruit();
+                    eatTimer = Time.time + eatTimeThreshold;
+                    killingBush = true;
+                }
+            }
+            else if (killingBush)
+            {
+                if (Time.time > eatTimer)
+                {
+                    bushFruitsTarget.EatBushFruits();
+                    killingBush = false;
+
+                    SearchForTarget();
+                }
+            }
+            else
+            {
+                SearchForTarget();
+            }
+
+            if(bushFruitsTarget)
+                {
+                if (bushFruitsTarget.transform.position.x < transform.position.x)
+                {
+                    left = true;
+                }
+                else
+                {
+                    left = false;
+                }
+            }
+
+          
+
+            if (!bushFruitsTarget)
+                {
+                SearchForTarget();
+            }
+        }
+        else
+        {
+            if (Vector2.Distance(transform.position, artifact.transform.position) > 1.5f)
+            {
+                float step = moveSpeed * Time.deltaTime;
+
+                transform.position = Vector2.MoveTowards(transform.position, artifact.transform.position, step);
+                isMoving = true;
+            }
+            else if (!isAttacking)
+            {
+                isAttacking = true;
+                attackTimer = Time.time + attackTimeThreshold;
+                isMoving = false;
+            }
+
+            if (isAttacking)
+            {
+                if (Time.time > attackTimer)
+                {
+                    Attack();
+                    attackTimer = Time.time + attackTimeThreshold;
+                }
+            }
+
+            if (artifact.transform.position.x < transform.position.x)
+            {
+                left = true;
+            }
+            else
+            {
+                left = false;
+            }
+        }
     }
 
     void SearchForTarget()
